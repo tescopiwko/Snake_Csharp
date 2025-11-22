2918210fe791127d4ee4aab95d68eea69c6573e8
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-11-22 15:32:35 +0100

    Cely zakladni had krome, nefungujiciho narazeni do sebe

--- Snake_C#/Game1.cs
+++ Snake_C#/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Snake_C_
 {
@@ -8,6 +10,17 @@ namespace Snake_C_
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D pixel;
+
+        List<Point> snake = new List<Point>();
+        Point direction = new Point(1, 0);
+
+        Point food;
+
+        int cellSize = 20;
+        int updateSpeed = 60;
+        double timer = 0;
 
         public Game1()
         {
@@ -28,6 +41,15 @@ namespace Snake_C_
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
+
+            snake.Clear();
+            snake.Add(new Point(10, 10));
+            snake.Add(new Point(9, 10));
+            snake.Add(new Point(8, 10));
+
+            SpawnFood();
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,7 +58,41 @@ namespace Snake_C_
                 Exit();
 
             // TODO: Add your update logic here
+            var k  = Keyboard.GetState();
 
+            if (k.IsKeyDown(Keys.Up) && direction != new Point(0,1))
+                direction = new Point(0, -1);
+
+            if (k.IsKeyDown(Keys.Down) && direction != new Point(0,-1))
+                direction = new Point(0, 1);
+
+            if (k.IsKeyDown(Keys.Left) && direction != new Point(1, 0))
+                direction = new Point(-1, 0);
+
+            if (k.IsKeyDown(Keys.Right) && direction != new Point(-1, 0))
+                direction = new Point(1, 0);
+
+            timer += gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer >= updateSpeed)
+            {
+                timer = 0;
+
+                Point newHead = new Point(snake[0].X + direction.X, snake[0].Y + direction.Y);
+
+                snake.Insert(0, newHead);
+
+                if (newHead == food)
+                {
+                    SpawnFood();
+                }
+                else
+                {
+                    snake.RemoveAt(snake.Count - 1);
+                }
+
+                CheckCollisions();
+            }
             base.Update(gameTime);
         }
 
@@ -45,8 +101,50 @@ namespace Snake_C_
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            foreach (var part in snake)
+            {
+                _spriteBatch.Draw(pixel, new Rectangle(part.X * cellSize, part.Y * cellSize, cellSize, cellSize), Color.Green);
+            }
+            _spriteBatch.Draw(pixel, new Rectangle(food.X * cellSize, food.Y * cellSize, cellSize, cellSize), Color.Red);
+            base.Draw(gameTime);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+        void SpawnFood()
+        {
+            Random rnd = new Random();
+            int maxX = _graphics.PreferredBackBufferWidth / cellSize;
+            int maxY = _graphics.PreferredBackBufferHeight / cellSize;
+
+            food = new Point(rnd.Next(0, maxX), rnd.Next(0, maxY));
+        }
+        void CheckCollisions()
+        {
+            int maxX = _graphics.PreferredBackBufferWidth / cellSize;
+            int maxY = _graphics.PreferredBackBufferHeight / cellSize;
+
+            if (snake[0].X < 0 || snake[0].X >= maxX ||
+                snake[0].Y < 0 || snake[0].Y >= maxY)
+            {
+                ResetGame();
+                return;
+            }
+        }
+
+        void ResetGame()
+        {
+            snake.Clear();
+            snake.Add(new Point(10, 10));
+            snake.Add(new Point(9, 10));
+            snake.Add(new Point(8, 10));
+            direction = new Point(1, 0);
+            SpawnFood();
+        }
     }
 }
+        
+
